5a88e80e55b4a6e73ba5be27432cbffbfc999226
Zac <32443586+ZacCampos@users.noreply.github.com>
2018-04-13 11:44:02 -0700

    Create header.h

---
+++ header.h
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *AUTHOR	 	: Farah Alfarah & Zac Campos
+ *STUDENT ID	: 1035501       & 1058476
+ *LAB #23		: Functions - Rock Paper Scissors
+ *CLASS		    : CS1A
+ *SECTION		: MW: 8AM
+ *DUE DATE		: 4/16/18
+*******************************************************************************/
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <time.h>
+#include <cstring>
+
+using namespace std;
+
+void GetInput(string &name, int &roundPref);
+char GetAndCheckPlay(char &playerChoice);
+
+
+
+
+
+
+
+
+
+#endif /* HEADER_H_ */
