4aae45d219dca886e1d0ccefc842ffff11e3fb58
Zac <32443586+ZacCampos@users.noreply.github.com>
2018-04-15 23:17:08 -0700

    Create Output3.cpp

---
+++ Output3.cpp
@@ -0,0 +1,44 @@
+***************************************************
+*  PROGRAMMED BY : Zac Campos & Farah Alfarah
+*  CLASS         : CS1A
+*  SECTION       : MW: 8am
+*  LAB #23       : Functions - Rock Paper Scissors
+***************************************************
+
+Enter Player's Name: Farah Alfarah
+Enter Number of Rounds in Match: 2
+
+**********************
+** CHOOSE YOUR PLAY **
+**********************
+R - Rock
+P - Paper
+S - Scissors
+Enter your play: p
+
+Farah Alfarah chooses PAPER!
+Computer chooses SCISSORS!
+
+Computer wins, better luck next time, Farah Alfarah!
+
+
+**********************
+** CHOOSE YOUR PLAY **
+**********************
+R - Rock
+P - Paper
+S - Scissors
+Enter your play: s
+
+Farah Alfarah chooses SCISSORS!
+Computer chooses SCISSORS!
+
+Computer wins, better luck next time, Farah Alfarah!
+
+
+***********************************
+*********  FINAL RESULTS  *********
+***********************************
+
+The user lost!!
+Farah Alfarah lost 100.00% of the time!
