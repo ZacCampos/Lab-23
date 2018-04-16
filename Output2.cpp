c2aa2bd30719c618fbf1b38d3706b0478e15beb4
Zac <32443586+ZacCampos@users.noreply.github.com>
2018-04-15 23:16:33 -0700

    Create Output2.cpp

---
+++ Output2.cpp
@@ -0,0 +1,86 @@
+***************************************************
+*  PROGRAMMED BY : Zac Campos & Farah Alfarah
+*  CLASS         : CS1A
+*  SECTION       : MW: 8am
+*  LAB #23       : Functions - Rock Paper Scissors
+***************************************************
+
+Enter Player's Name: Zac Campos
+Enter Number of Rounds in Match: 4
+
+**********************
+** CHOOSE YOUR PLAY **
+**********************
+R - Rock
+P - Paper
+S - Scissors
+Enter your play: asdf
+
+** INVALID INPUT - Please Enter (R, P, or S) **
+
+R - Rock
+P - Paper
+S - Scissors
+Enter your play: P
+
+Zac Campos chooses PAPER!
+Computer chooses PAPER!
+
+Computer wins, better luck next time, Zac Campos!
+
+
+**********************
+** CHOOSE YOUR PLAY **
+**********************
+R - Rock
+P - Paper
+S - Scissors
+Enter your play: A
+
+** INVALID INPUT - Please Enter (R, P, or S) **
+
+R - Rock
+P - Paper
+S - Scissors
+Enter your play: S
+
+Zac Campos chooses SCISSORS!
+Computer chooses SCISSORS!
+
+Computer wins, better luck next time, Zac Campos!
+
+
+**********************
+** CHOOSE YOUR PLAY **
+**********************
+R - Rock
+P - Paper
+S - Scissors
+Enter your play: p
+
+Zac Campos chooses PAPER!
+Computer chooses PAPER!
+
+Computer wins, better luck next time, Zac Campos!
+
+
+**********************
+** CHOOSE YOUR PLAY **
+**********************
+R - Rock
+P - Paper
+S - Scissors
+Enter your play: s
+
+Zac Campos chooses SCISSORS!
+Computer chooses PAPER!
+
+Zac Campos WINS!!
+
+
+***********************************
+*********  FINAL RESULTS  *********
+***********************************
+
+The user lost!!
+Zac Campos lost 75.00% of the time!
